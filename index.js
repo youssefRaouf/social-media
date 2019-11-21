de24c9abbf7fbca9bef0186b494e12d8ebcc21d3
Youssef <youssefraouf32@gmail.com>
2019-11-21 20:25:20 +0200

    adding prisma client and connecting my graphql to a demo database

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { GraphQLServer } = require('graphql-yoga')
-
+const { prisma } = require("./src/generated/prisma-client");
 // 1
 const typeDefs = `
 type Query {
@@ -8,7 +8,7 @@ type Query {
 }
 type Mutation {
     post(name: String!, email: String!): User!
-    deleteUser(id:ID!):String!
+    deleteUser(name:String!):String!
     updateUser(id:ID!,name:String,email:String):User
   }
 
@@ -27,20 +27,18 @@ let users = [
     Query: {
       info: () => `This is the API of a Hackernews Clone`,
       // 2
-      users: () => users,
+      users: (root, args, context, info) => {
+        return context.prisma.users();
+      }
     },
     // 3
     Mutation: {
         // 2
-        post: (parent,args) => {
-           const user = {
-            id: `${idCount++}`,
-            name: args.name,
-            email: args.email,
-          }
-
-          users.push(user)
-          return user
+        post:(root,args,context,info)=>{
+          return context.prisma.createUser({
+           name:args.name ,
+           email:args.email
+          });
         },
         updateUser: (parent,args) => {
             const user = {
@@ -48,24 +46,25 @@ let users = [
              name: args.name,
              email: args.email,
            }
-           for (let i=0; i<users.length; i++) {
-            if(args.id==users[i].id){
+           for (let i=0; i<context.prisma.users.length; i++) {
+            if(args.id==context.prisma.users[i].id){
                 users[i]=user;
             }
           }
            return user
          },
-         deleteUser: (parent,args) => {
+         deleteUser: (parent,args,context) => {
             let users1=[]
-           for (let i=0; i<users.length; i++) {
-            if(args.id!=users[i].id){
-                users1.push(users[i]);
-            }
-          }
-          if(users.length==users1.length){
-              return "user not found"
-          }
-           users=users1;
+            console.log(context.prisma.users().name,"r23r23r234234");
+          //  for (let i=0; i<context.prisma.users.length; i++) {
+          //   if(args.name!=context.prisma.users[i].name){
+          //       users1.push(users[i]);
+          //   }
+          // }
+          // if(users.length==users1.length){
+          //     return "user not found"
+          // }
+          //  context.prisma.users=users1;
            return "deleted succesfully"
          }
       },
@@ -74,5 +73,6 @@ let users = [
 const server = new GraphQLServer({
   typeDefs,
   resolvers,
+  context:{prisma}
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
